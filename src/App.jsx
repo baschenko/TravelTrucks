45f5be293ba212c29e37fb32ee2314311b70155c
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2025-01-15 01:56:20 +0200

    add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  // lazy,
+  Suspense,
+  // useEffect
+} from 'react';
+import { Route, Routes } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { refreshUser } from '../redux/auth/operations';
+// import { selectIsRefreshing } from '../redux/auth/selectors';
+import Layout from './components/Layout/Layout.jsx';
+import CatalogPage from './pages/CatalogPage/CatalogPage.jsx';
+import CampersDetailsPage from './pages/CampersDetailsPage/CampersDetailsPage.jsx';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx';
+import CampersFeatures from './components/CampersFeatures/CampersFeatures.jsx';
+import CampersReviews from './components/CampersReviews/CampersReviews.jsx';
+import HomePage from './pages/HomePage/HomePage.jsx';
+// import RestrictedRuote from './RestrictedRuote';
+// import PrivateRoute from './PrivateRoute';
 
-function App() {
-  const [count, setCount] = useState(0)
+// const HomePage = lazy(() => import('../pages/HomePage/HomePage.jsx'));
+// const RegistrationPage = lazy(() =>
+// import('../pages/RegistrationPage/RegistrationPage')
+// );
+// const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
+// const ContactsPage = lazy(() => import('../pages/ContactsPage/ContactsPage'));
+
+export default function App() {
+  // const dispatch = useDispatch();
+  // const isRefreshing = useSelector(selectIsRefreshing);
+
+  // useEffect(() => {
+  //   // dispatch(refreshUser());
+  // }, [dispatch]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Layout>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route index element={<HomePage />} />
 
-export default App
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="catalog/:id" element={<CampersDetailsPage />}>
+            <Route path="features" element={<CampersFeatures />} />
+            <Route path="reviews" element={<CampersReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </Layout>
+  );
+}
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import AppBar from '../AppBar/AppBar.jsx';
+import css from './Layout.module.css';
+
+export default function Layout({ children }) {
+  return (
+    <div className={css.container}>
+      <AppBar />
+      {children}
+    </div>
+  );
+}
