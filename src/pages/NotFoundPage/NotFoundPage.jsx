228919165035b472cd52f08ee114181bf6903ba0
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2025-01-20 00:41:43 +0200

    add 404

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+import css from './NotFoundPage.module.css';
+
 const NotFoundPage = () => {
-  return <div>404 - not found</div>;
+  return (
+    <div className={css.container}>
+      <h2 className={css.title}>404 - not found</h2>
+      <p className={css.text}>
+        Unfortunately, there is no page at this link, let`s try from the home
+        page
+      </p>
+      <Link to="/" className={css.btn}>
+        Return to Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFoundPage;
