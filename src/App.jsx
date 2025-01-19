e967e1070a0baacb905c93c7d51b3e3c9b0777a3
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2025-01-19 17:26:05 +0200

    clean app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import {
   // lazy,
   Suspense,
-  // useEffect
 } from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { refreshUser } from '../redux/auth/operations';
-// import { selectIsRefreshing } from '../redux/auth/selectors';
 import Layout from './components/Layout/Layout.jsx';
 import CatalogPage from './pages/CatalogPage/CatalogPage.jsx';
 import CampersDetailsPage from './pages/CampersDetailsPage/CampersDetailsPage.jsx';
