907324e1965bed9e31249bf36a870fc3a989b81e
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2025-01-19 18:55:39 +0200

    add modal img

---
+++ src/components/ImageModal/ImageModal.jsx
@@ -0,0 +1,41 @@
+import Modal from 'react-modal';
+import css from './ImageModal.module.css';
+import Icon from '../Icon/Icon.jsx';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  overlay: { backgroundColor: 'rgba(0,0,0,0.6)' },
+};
+
+Modal.setAppElement('#root');
+
+const ImageModal = ({ modalIsOpen, closeModal, src }) => {
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      style={customStyles}
+      contentLabel="Example Modal"
+    >
+      <div className={css.modalHeader}>
+        <button
+          aria-label="Close modal button"
+          className={css.closeModalBtn}
+          onClick={closeModal}
+        >
+          <Icon id="icon-close" width={14} height={14} />
+        </button>
+      </div>
+      <img src={src} />
+    </Modal>
+  );
+};
+
+export default ImageModal;
--- src/pages/CampersDetailsPage/CampersDetailsPage.jsx
+++ src/pages/CampersDetailsPage/CampersDetailsPage.jsx
@@ -2,15 +2,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Outlet, useParams } from 'react-router-dom';
 import RatingAndLocation from '../../components/RatingAndLocation/RatingAndLocation.jsx';
 import Price from '../../components/Price/Price.jsx';
-import { Suspense, useEffect } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { InfinitySpin } from 'react-loader-spinner';
 import { getCamperInfo } from '../../redux/campers/operations.js';
 import { selectCamper } from '../../redux/campers/selectors.js';
 import css from './CampersDetailsPage.module.css';
 import BookingForm from '../../components/BookingForm/BookingForm.jsx';
+import ImageModal from '../../components/ImageModal/ImageModal.jsx';
 
 const CampersDetailsPage = () => {
   const { camperId } = useParams();
+
+  const [showModal, setShowModal] = useState(false);
+  const [modalUrl, setModalUrl] = useState('');
 
   const dispatch = useDispatch();
 
@@ -19,6 +23,16 @@ const CampersDetailsPage = () => {
   }, [camperId, dispatch]);
 
   const camper = useSelector(selectCamper);
+
+  const openModal = url => {
+    setShowModal(true);
+    setModalUrl(url);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setModalUrl('');
+  };
 
   return (
     camper && (
@@ -39,6 +53,9 @@ const CampersDetailsPage = () => {
                 // width="292"
                 // height="312"
                 className={css.img}
+                onClick={() => {
+                  openModal(picture.original);
+                }}
               />
             </li>
           ))}
@@ -80,6 +97,11 @@ const CampersDetailsPage = () => {
           </div>
           <BookingForm />
         </div>
+        <ImageModal
+          modalIsOpen={showModal}
+          closeModal={closeModal}
+          src={modalUrl}
+        />
       </div>
     )
   );
